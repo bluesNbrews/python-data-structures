15063a9b14b272047a2d2d653068478a4f013440
Steven <mstevenwilliams@gmail.com>
2021-01-30 19:24:42 -0600

    Add test cases.

--- active_directory.py
+++ active_directory.py
@@ -31,44 +31,65 @@ def is_user_in_group(user, group):
       user(str): user name/id
       group(class:Group): group to check user membership against
     """
+    #Handle cases for no user or no group
+    if user == "":
+        return "no user provided"
+
+    if group == "":
+        return "no group provided"
+
     group_list = group.get_groups()
     for g in group_list:
-        print(g)
         if not g.groups:
-            print("Empty list")
             user_list = g.get_users()
             for u in user_list:
-                print(u)
                 if u == str(user):
-                    print("True")
                     result = True
                 else:
-                    print("False")
                     result = False
             return result
         if isinstance(g, Group):
             result = is_user_in_group(user, g)
     return result
 
-parent = Group("parent")
-child_a = Group("child_a")
-child_b = Group("child_b")
-sub_child_1 = Group("subchild1")
-sub_child_2 = Group("subchild2")
+if __name__ == "__main__":
 
-sub_child_user_a = "sub_child_user_a"
-sub_child_user_b = "sub_child_user_b"
-sub_child_user_c = "sub_child_user_c"
+    parent = Group("parent")
+    child_a = Group("child_a")
+    child_b = Group("child_b")
+    sub_child_1 = Group("subchild1")
+    sub_child_2 = Group("subchild2")
 
-parent.add_group(child_a)
-parent.add_group(child_b)
+    sub_child_user_a = "sub_child_user_a"
+    sub_child_user_b = "sub_child_user_b"
+    sub_child_user_c = "sub_child_user_c"
 
-child_a.add_group(sub_child_1)
-child_b.add_group(sub_child_2)
+    parent.add_group(child_a)
+    parent.add_group(child_b)
 
-sub_child_1.add_user(sub_child_user_a)
-sub_child_1.add_user(sub_child_user_b)
-sub_child_2.add_user(sub_child_user_c)
+    child_a.add_group(sub_child_1)
+    child_b.add_group(sub_child_2)
 
-search_result = is_user_in_group("sub_child_user_c", parent)
-print("The search result is {}".format(search_result))
+    sub_child_1.add_user(sub_child_user_a)
+    sub_child_1.add_user(sub_child_user_b)
+    sub_child_2.add_user(sub_child_user_c)
+
+    #Test Case 1
+    search_result = is_user_in_group("sub_child_user_c", parent)
+    print("The search result is {}.".format(search_result))
+    #Expected Output: The search result is True.
+
+    #Test Case 2
+    search_result = is_user_in_group("sub_child_user_d", parent)
+    print("The search result is {}.".format(search_result))
+    #Expected Output: The search result is False.
+
+    #Test Case 3
+    search_result = is_user_in_group("", parent)
+    print("The search result is {}.".format(search_result))
+    #Expected Output: The search result is no user provided.
+
+    #Test Case 4
+    search_result = is_user_in_group("sub_child_user_c", "")
+    print("The search result is {}.".format(search_result))
+    #Expected Output: The search result is no group provided.
